a72909403ef78da4ae8c2b734e9c1c766bd4db48
Rick Henry <fredericmhenry@gmail.com>
2018-04-05 20:17:56 -0400

    Thu Apr  5 20:17:56 EDT 2018 update dotfiles M  config/fish/functions/update_dotfiles.fish

--- config/fish/functions/update_dotfiles.fish
+++ config/fish/functions/update_dotfiles.fish
@@ -2,13 +2,14 @@ function update_dotfiles
 	set -l prevdir $PWD
 echo "Updating Dotfiles"
 cd ~/.dotfiles
-if test ! -z (git status -s) 
-git add -a
-git commit -sm "update dotfiles (date) (git status -s)"
+if git status -s 
+git add -A
+git commit -m "update dotfiles" (date) (git status -s)""
 git pull
 git push
 else
 git pull
 end
 ./install
+cd $prevdir
 end
