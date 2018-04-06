c34863c7ff5eabf1ff8a353eb56023f0f22bf8c9
Rick Henry <fredericmhenry@gmail.com>
2018-04-06 14:32:33 -0400

    Fri Apr  6 14:32:33 EDT 2018 update dotfiles M  config/fish/functions/new_brahms_remote.fish

--- config/fish/functions/new_brahms_remote.fish
+++ config/fish/functions/new_brahms_remote.fish
@@ -1,8 +1,11 @@
+# Defined in /tmp/fish.Si6HwI/new_brahms_remote.fish @ line 2
 function new_brahms_remote
-  set -l newrepo $argv[1]
+	set -l prevdir $PWD
+	set -l newrepo $argv[1]
   mkdir $newrepo
   git init $newrepo
-  ssh rick@brahms.local "git init --bare /home/rick/remotes/$newrepo.git"
+  ssh rick@brahms "git init --bare /home/rick/remotes/$newrepo.git"
   cd $newrepo
-  git remote add origin "rick@brahms.local:remotes/$newrepo.git"
+  git remote add origin "rick@brahms:remotes/$newrepo.git"
+  cd $prevdir
 end
