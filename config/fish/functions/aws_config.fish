8e08f30a8b1b44cb4c2eac2d238e9c90f4f33cb2
Rick Henry <fredericmhenry@gmail.com>
2018-04-05 21:55:06 -0400

    Thu Apr  5 21:55:06 EDT 2018 update dotfiles M  config/fish/functions/aws_config.fish

--- config/fish/functions/aws_config.fish
+++ config/fish/functions/aws_config.fish
@@ -1,8 +1,9 @@
+# Defined in /tmp/fish.YAsgm2/aws_config.fish @ line 2
 function aws_config
 	mkdir ~/.aws
-echo "[default]" > ~/.aws/credentials
-echo "aws_access_key_id = (pass show aws/primary/ID)" >> ~/.aws/credentials
-echo "aws_secret_access_key = (pass show aws/primary/secret)" >> ~/.aws/credentials
-echo "" >> ~/.aws/credentials
-echo "# vim: se ft=dosini:" >> ~/.aws/credentials
+  echo "[default]" > ~/.aws/credentials
+  echo "aws_access_key_id =" (pass show aws/primary/ID) >> ~/.aws/credentials
+  echo "aws_secret_access_key =" (pass show aws/primary/secret) >> ~/.aws/credentials
+  echo "" >> ~/.aws/credentials
+  echo "# vim: se ft=dosini:" >> ~/.aws/credentials
 end
