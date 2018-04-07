90b7f673ae4b371c4abe547e9d0ac2feaf40be8f
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 17:25:31 -0400

    update dotbotify
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- config/fish/functions/dotbotify.fish
+++ config/fish/functions/dotbotify.fish
@@ -1,5 +1,10 @@
-# Defined in /tmp/fish.st3sjG/dotbotify.fish @ line 2
+# Defined in /tmp/fish.0NWGhs/dotbotify.fish @ line 2
 function dotbotify --description 'add file to dotbot'
+	if not set -q argv[2]
+    echo "Missing Argument"
+    echo "dotbotify [OLD PATH] [DOTBOT PATH]"
+    return 2
+  end
 	set -l prevdir $PWD
   set -l oldpath (readlink -f $argv[1] | sed "s@$HOME@\~@")
   set -l newpath (echo $argv[2] | sed 's@.*/.dotfiles/@@ ; s@"@@')
