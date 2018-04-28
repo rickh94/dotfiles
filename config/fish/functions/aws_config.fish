432575ee340a78a7f31315de6ed1f33b748d4cef
Rick Henry <fredericmhenry@gmail.com>
2018-04-28 13:19:12 -0400

    Sat Apr 28 13:19:12 EDT 2018 update dotfiles M  config/fish/functions/aws_config.fish

--- config/fish/functions/aws_config.fish
+++ config/fish/functions/aws_config.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.7ONgmE/aws_config.fish @ line 2
+# Defined in /tmp/fish.AJFzP3/aws_config.fish @ line 2
 function aws_config
-	/bin/mkdir ~/.aws
+	/bin/mkdir ~/.aws ^/dev/null
   echo "[default]" > ~/.aws/credentials
   echo "aws_access_key_id =" (pass show aws/primary/ID) >> ~/.aws/credentials
   echo "aws_secret_access_key =" (pass show aws/primary/secret) >> ~/.aws/credentials
