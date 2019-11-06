868e2bbd7d4939a1fd6e615f353afe8b63e95079
Rick Henry <rickhenry@rickhenry.dev>
2019-11-05 22:40:15 -0500

    Tue 05 Nov 2019 10:40:15 PM EST update dotfiles M  bashrc Tue 05 Nov 2019 10:40:15 PM EST update dotfiles M  config/fish/config.fish Tue 05 Nov 2019 10:40:15 PM EST update dotfiles A  config/fish/functions/vim.fish Tue 05 Nov 2019 10:40:15 PM EST update dotfiles M  config/omf/bundle Tue 05 Nov 2019 10:40:15 PM EST update dotfiles M  gitconfig Tue 05 Nov 2019 10:40:15 PM EST update dotfiles M  install.conf.yaml Tue 05 Nov 2019 10:40:15 PM EST update dotfiles A  vuerc

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,5 @@
 set -x GOROOT /usr/lib/go
 set -x GOPATH $HOME/go:/usr/lib/go
-set -x SSH_ASKPASS (which ksshaskpass)
 
 set -x VISUAL 'nvim'
 set -x EDITOR 'nvim'
@@ -21,16 +20,16 @@ end
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+#[ -f /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
 # tabtab source for sls package
 # uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+#[ -f /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /home/rick/.node_modules/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
 # tabtab source for slss package
 # uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /home/rick/.config/yarn/global/node_modules/tabtab/.completions/slss.fish ]; and . /home/rick/.config/yarn/global/node_modules/tabtab/.completions/slss.fish
+#[ -f /home/rick/.config/yarn/global/node_modules/tabtab/.completions/slss.fish ]; and . /home/rick/.config/yarn/global/node_modules/tabtab/.completions/slss.fish
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/rick/.pyenv/versions/anaconda3-2019.03/bin/conda "shell.fish" "hook" $argv | source
+#eval /home/rick/.pyenv/versions/anaconda3-2019.03/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
---
+++ config/fish/functions/vim.fish
@@ -0,0 +1,3 @@
+function vim
+	nvim $args
+end
