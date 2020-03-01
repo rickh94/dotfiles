a8c440c9229962f9ab2a46c523bf0c515b3efcd9
Rick Henry <rickhenry@rickhenry.dev>
2020-03-01 10:33:13 -0500

    Sun 01 Mar 2020 10:33:12 AM EST update dotfiles M  config/fish/config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,13 +7,6 @@ set -x PAGER 'less'
 
 fish_default_key_bindings
 
-if test -f /usr/bin/pass
-  set -x BORG_PASSPHRASE (pass show borg/(hostname))
-end
-
-if test -f $HOME/.borg_repo
-  set -x BORG_REPO (cat $HOME/.borg_repo)
-end
 
 
 # vim: se wrap:
