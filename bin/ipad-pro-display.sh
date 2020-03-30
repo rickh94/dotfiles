f84e0907ef84acde456374aa0f9b765709f9bfac
Rick Henry <rickhenry@rickhenry.dev>
2020-03-30 15:23:23 -0400

    add ipad pro display
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

---
+++ bin/ipad-pro-display.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+IPADDISPLAY=""
+PRIMARYDISPLAY=""
+
+if [ "`hostname`" = "purcell" ]; then
+  IPADDISPLAY="HDMI1"
+  PRIMARYDISPLAY="eDP1"
+elif ["`hostname`" = "beethoven"]; then
+  IPADDISPLAY="HDMI-A-0"
+  PRIMARYDISPLAY="DisplayPort-0"
+else
+  echo "No configuration for this host"
+  exit 1
+fi
+if [ -n "`xrandr | grep ipad_pro | grep '*'`" ]; then
+  xrandr --output $IPADDISPLAY --off
+else
+  xrandr --output $IPADDISPLAY --mode ipad_pro --left-of $PRIMARYDISPLAY
+fi
