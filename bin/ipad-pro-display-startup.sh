fd8da1e83f422936dc9fd3740c5110f0dcc02ac7
Rick Henry <rickhenry@rickhenry.dev>
2020-04-05 16:12:35 -0400

    Sun 05 Apr 2020 04:12:35 PM EDT update dotfiles M  bin/ipad-pro-display-startup.sh

--- bin/ipad-pro-display-startup.sh
+++ bin/ipad-pro-display-startup.sh
@@ -5,5 +5,6 @@ if [ "`hostname`" = "purcell" ]; then
   IPADDISPLAY="HDMI1"
 elif [ "`hostname`" = "beethoven" ]; then
   IPADDISPLAY="HDMI-A-0"
+  xrandr --output DisplayPort-0 --set TearFree on --mode 3440x1440
 fi
 xrandr --output $IPADDISPLAY --off
